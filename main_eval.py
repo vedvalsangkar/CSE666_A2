c8b1b85224c63644448f85dc7f9bec294825efe9
Ved Harish Valsangkar <vedvalsangkar@gmail.com>
2019-03-17 19:22:08 -0400

    Individual file evaluation.
    Signed-off-by: ved <ved28592@gmail.com>

--- main_eval.py
+++ main_eval.py
@@ -9,6 +9,7 @@
 import torch
 import numpy as np
 import pandas as pd
+import argparse as ap
 
 from PIL import Image
 from sklearn.metrics.pairwise import cosine_similarity
@@ -179,46 +180,141 @@ def evaluate(filename):
     print("\n\nDone")
 
 
+def single_eval(filename, split):
+
+    device = torch.device("cuda:0" if cuda.is_available() else "cpu")
+
+    helper = Helper()
+
+    testing_set, testing_loader = helper.get_data(mode="test", testing_batch_size=1)
+
+    print("Starting single file evaluation")
+
+    to_tensor = transforms.ToTensor()
+
+    true_val = {}
+    score = {}
+    tpr = {}
+    fpr = {}
+    thresh = {}
+    area = {}
+
+    set_n = split
+
+    true_val[set_n] = []
+    score[set_n] = []
+
+    model = load_model(filename + str(set_n) + ".pt", device)
+
+    model.eval()
+
+    total_len = len(testing_loader[set_n])
+
+    with torch.no_grad():
+        for i, (list_1, list_2, labels) in enumerate(testing_loader[set_n]):
+            try:
+                if type(list_1).__name__ != 'list' or type(list_2).__name__ != 'list':
+                    print("Issues with testing file at location {0}".format(i))
+                    print(list_1)
+                    print(list_2)
+                    continue
+
+                l1_avg = np.zeros([1, model.features])
+                l1 = 0
+
+                l2_avg = np.zeros([1, model.features])
+                l2 = 0
+
+                for im in list_1:
+                    try:
+                        image = Image.open(im[0])
+                        tensor_img = to_tensor(image).to(device)
+                        output = model(tensor_img.unsqueeze(0))
+                        l1_avg += output.cpu().numpy()
+                        l1 += 1
+                    except FileNotFoundError:
+                        print("File {0} not found. Skipping.".format(im))
+                l1_avg /= l1
+
+                for im in list_2:
+                    try:
+                        image = Image.open(im[0])
+                        tensor_img = to_tensor(image).to(device)
+                        output = model(tensor_img.unsqueeze(0))
+                        l2_avg += output.cpu().numpy()
+                        l2 += 1
+                    except FileNotFoundError:
+                        print("File {0} not found. Skipping.".format(im))
+                l2_avg /= l2
+
+                s = cosine_similarity(l1_avg.reshape(1, -1), l2_avg.reshape(1, -1))[0, 0]
+                score[set_n].append(s)
+                true_val[set_n].append(labels.item())
+                if (i + 1) % 500 == 0:
+                    print("Step: {0}/{1}".format(i, total_len))
+
+            except ValueError:
+                print("Issues with testing file at location {0}".format(i))
+                print(list_1)
+                print(list_2)
+            #     print(score[1][i], true_val[1][i])
+
+        # Code to evaluate ROC graph is taken from the official documentation.
+        # https://scikit-learn.org/stable/auto_examples/model_selection/plot_roc.html#sphx-glr-auto-examples-model-selection-plot-roc-py
+
+        fpr[set_n], tpr[set_n], thresh[set_n] = roc_curve(np.asarray(true_val[set_n]), np.asarray(score[set_n]))
+        area[set_n] = auc(fpr[set_n], tpr[set_n])
+
+        plt.figure()
+        plt.plot(fpr[set_n],
+                 tpr[set_n],
+                 color='darkorange',
+                 lw=2,
+                 label="ROC curve (area = {0:.2f})".format(area[set_n]))
+
+        plt.xlim([0.0, 1.05])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Receiver Operating Characteristic for Split {0}'.format(set_n))
+        plt.legend(loc="lower right")
+        plt.savefig(fname="images/{1}ROC{0}.jpg".format(set_n, filename[:-1]))
+
+        pd.DataFrame(true_val).to_csv(path_or_buf="results/{0}GT_{1}.csv".format(filename[:-1], set_n))
+        pd.DataFrame(score).to_csv(path_or_buf="results/{0}Score_{1}.csv".format(filename[:-1], set_n))
+        pd.DataFrame(tpr).to_csv(path_or_buf="results/{0}TPR_{1}.csv".format(filename[:-1], set_n))
+        pd.DataFrame(fpr).to_csv(path_or_buf="results/{0}FPR_{1}.csv".format(filename[:-1], set_n))
+        pd.DataFrame(thresh).to_csv(path_or_buf="results/{0}TH_{1}.csv".format(filename[:-1], set_n))
+        pd.DataFrame(area).to_csv(path_or_buf="results/{0}Area_{1}.csv".format(filename[:-1], set_n))
+
+
 if __name__ == "__main__":
 
-    f = open("latest_t_stmp.txt", 'r')
-    latest_file = f.readline()
-    f.close()
+    parser = ap.ArgumentParser()
 
-    print(latest_file)
+    parser.add_argument("-f",
+                        "--file",
+                        type=str,
+                        # default="",
+                        help="File to process. If None, all will be processed.")
 
-    evaluate(latest_file)
+    parser.add_argument("-s",
+                        "--split",
+                        type=int,
+                        default=2,
+                        help="Split number to process.")
 
+    args = parser.parse_args()
 
+    if args.file is None:
+        f = open("latest_t_stmp.txt", 'r')
+        latest_file = f.readline()
+        f.close()
 
-#         try:
-#             dot = np.sum(l1_avg * l2_avg)
-#             a = np.sqrt(np.sum(l1_avg * l1_avg))
-#             b = np.sqrt(np.sum(l2_avg * l2_avg))
-#             # b = np.linalg.norm(l2_avg)
-#             cos_sim = dot / (a * b)
-#             if np.isnan(cos_sim):
-#                 print("L1", l1_avg)
-#                 print("L2", l2_avg)
-#         except ValueError:
-#             print("L1", list_1)
-#             print("L2", list_2)
-#             continue
-#         # https://stackoverflow.com/questions/18424228/cosine-similarity-between-2-number-lists
-#
-#         total += 1
-#         allow = cos_sim > threshold
-#         # correct += int(cos_sim > threshold and labels.item())
-#         correct += int((allow and labels.item()) or (not allow and not labels.item()))
-#         false_reject += int(not allow and labels.item())
-#         false_accept += int(allow and not labels.item())
-#         print(cos_sim, cos_sim > threshold, labels.item())
-#
-#         if (i+1) % 10 == 0:
-#             print("Step {0}/{1}. C|FA|FR:{2}|{3}|{4}<T>:{5}".format(i, total_len, correct,
-#                                                                     false_accept, false_reject, total))
-#
-# print("Accuracy metrics:\nTotal{0}".format(total))
-# print("Correct      : {0} \t|\t {1} %".format(correct, (correct / total)))
-# print("False Accept : {0} \t|\t {1} %".format(false_accept, (false_accept / total)))
-# print("False Reject : {0} \t|\t {1} %".format(false_reject, (false_reject / total)))
+        print(latest_file)
+
+        evaluate(latest_file)
+
+    else:
+
+        single_eval(args.file, args.split)
